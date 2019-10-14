21894a95e76dc7294885f68166980d29a6a42aee
XAmi101 <famousvacancy@gmail.com>
2019-10-14 13:57:12 -0400

    commit

--- index.js
+++ index.js
@@ -18,6 +18,14 @@ server.get('/', (req, res) => {
 server.post('/api/register', (req, res) => {
   let user = req.body;
 
+/*
+// validate the user
+  // hash the password
+  const hash = bcrypt.hashSync(user.password, 8);
+  // we override the password with the hash
+  user.password = hash;
+*/
+
   Users.add(user)
     .then(saved => {
       res.status(201).json(saved);
@@ -27,21 +35,40 @@ server.post('/api/register', (req, res) => {
     });
 });
 
+
 server.post('/api/login', (req, res) => {
   let { username, password } = req.body;
 
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (user) {
-        res.status(200).json({ message: `Welcome ${user.username}!` });
-      } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+
+    if (username && password) {
+      Users.findBy({ username })
+        .first()
+        .then(user => {
+          if (user && bcrypt.compareSync(password, user.password)) {
+            res.status(200).json({ message: `Welcome ${user.username}!` });
+          } else {
+            res.status(401).json({ message: 'You cannot pass!!' });
+          }
+        })
+        .catch(error => {
+          res.status(500).json(error);
+        });
+    } else {
+      res.status(400).json({ message: 'please provide credentials' });
+    }
+
+//   Users.findBy({ username })
+//     .first()
+//     .then(user => {
+//       if (user) {
+//         res.status(200).json({ message: `Welcome ${user.username}!` });
+//       } else {
+//         res.status(401).json({ message: 'Invalid Credentials' });
+//       }
+//     })
+//     .catch(error => {
+//       res.status(500).json(error);
+//     });
 });
 
 server.get('/api/users', (req, res) => {
